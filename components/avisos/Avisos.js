3901fac188dd0d02c90e075353a1038e3e68cdb2
Maria Motoso Gomes <mariamotosogomes@gmail.com>
2020-12-09 11:19:27 -0300

    Selecionar e alterar uma aviso

--- components/avisos/Avisos.js
+++ components/avisos/Avisos.js
@@ -4,7 +4,7 @@
 
  /**
   * Inserir um aviso no banco de dados
-  * @param {object} aviso  O aviso deve estar no seguinte formato:
+  * @param {object} aviso O aviso deve estar no seguinte formato:
   * {titulo:<string>, data:<string>, mensagem:<string>}
   * @returns {object} Mensagem de sucesso ou de erro
   */
@@ -23,11 +23,27 @@
  } // fim do salvar
 
 /**
+ * Alterar um aviso no banco de dados
+ * @param {object} aviso O aviso deve estar no seguinte formato:
+ * {titulo:<string>, data:<string>, mensagem:<string>}
+ * @param {int} id ID do aviso
+ * @returns {object} Mensagem de sucesso ou de erro
+ */
+ function editar(aviso, id){
+   return db('avisos').where('ID_avisos',id).update(aviso)
+   .then(id => {
+    return { tipo: "sucesso", corpo: "Dados Alterados com sucesso!"}
+  })
+  .catch(erro =>{
+    return { tipo:"erro", corpo: "Erro: "+ erro }
+  })
+ }
+
+/**
  * Seleciona todos os aviso cadastrados
  * @returns {object} objeto com todos os avisos cadastrados
  * ou uma mensagem de erro
  */
-
 function selecionarTodos(){
   return db.select('*').from('avisos')
   .then(avisos => { return avisos })
@@ -35,12 +51,31 @@ function selecionarTodos(){
 }// fim do selecionarTodos
 
 /**
+ * Seleciona um aviso
+ * @param {*} id ID do aviso que será selecionado
+ * @returns {object} Objeto com o aviso selecionado
+ */
+function selecionarAviso(id){
+  return db.select('*').from('avisos').where('ID_avisos',id).first()
+  .then(aviso => {return aviso})
+  .catch (erro => {
+    return{tipo: "erro", corpo: "Erro: "+ erro }
+  })
+}
+
+/**
  * Função que exclui um aviso do banco de dados
  * @param {int} id id do aviso
  */
-
 function excluir(id){
   return db.del().from('avisos').where('ID_avisos',id)
 }
 
- module.exports = {salvar, selecionarTodos, excluir}
+ module.exports =
+  {
+    salvar, 
+    selecionarTodos, 
+    excluir, 
+    selecionarAviso,
+    editar
+  }
